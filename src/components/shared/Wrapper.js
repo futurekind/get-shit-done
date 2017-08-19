160cb0c77a0a66d9cc8e9751e8a1444e56c91252
futurekind <f.trinler@gmail.com>
2017-08-19 16:07:42 +0200

    Adding Wrapper component

---
+++ src/components/shared/Wrapper.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const View = styled.section`
+    max-width: 1024px;
+    margin: 0 auto;
+    padding: 24px;
+`
+
+const Wrapper = (props) => {
+    return (
+        <View>
+            { props.children }
+        </View>
+    )
+}
+
+export default Wrapper
